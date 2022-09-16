42467f2140f331224ebf8d550527beb5275ce34e
Danielle <daniellec0321@gmail.com>
2022-09-15 22:43:17 -0400

    brute.py can read in file and create array of information. Added gitignore

--- brute/brute.py
+++ brute/brute.py
@@ -1,5 +1,17 @@
-# Function to read file
+# Class "problem"
+class Problem:
 
+    def __init__(self, probNumber_in, maxLiterals_in, numVariables_in, numClauses_in, answer_in):
+        self.probNumber = probNumber_in
+        self.maxLiterals = maxLiterals_in
+        self.numVariables = numVariables_in
+        self.numClauses = numClauses_in
+        self.answer = answer_in
+        self.probArray = list()
+
+
+
+# Function to read file
 def readFile(filename):
     
     rFile = open(filename, "r")
@@ -23,18 +35,59 @@ def readFile(filename):
 
 
 
-# parse a list into arrays
-def parseFile(equation_list, numProblems):
-    return
+# parse a list into LIST OF CLASS PROBLEMS
+def getProblems(equation_list, numProblems):
+
+    probList = list()
+
+    # initialize class loop
+    celems = equation_list[0].split(" ")
+    pelems = equation_list[1].split(" ")
+
+    init = Problem(int(celems[1]), int(celems[2]), int(pelems[2]), int(pelems[3]), celems[3])
+
+    # loop through list and parse
+    counter = 0
+    while counter < len(equation_list):
+
+        celems = equation_list[counter].split(" ")
+        counter += 1
+        pelems = equation_list[counter].split(" ")
+        counter += 1
+
+        init = Problem(int(celems[1]), int(celems[2]), int(pelems[2]), int(pelems[3]), celems[3])
+
+        # read in problem array
+        while counter < counter+init.numClauses:
+
+            app_clause = list()
+
+            # get clause
+            clause_in = equation_list[counter].split(",")
+            counter += 1
+
+            # append everything to clause, EXCEPT FOR ZERO
+            for i in range(0, len(clause_in)-1):
+                app_clause.append(int(clause_in[i]))
+
+            # If length of clause is less than the maxLiterals + 1, append with zeros
+            if len(clause_in) < init.maxLiterals + 1:
+                for i in range(len(clause_in), init.maxLiterals+1):
+                    app_clause.append(0)
+
+            # append to current problem class
+            init.probArray.append(app_clause)
+
+        # append class problem to list
+        probList.append(init)
+
+    return probList
 
 
 
-# main function
-# get
-test, num = readFile("../tests/kSAT.txt")
-print(num)
-
-
+# initialize brute force
+equation_list, numProblems = readFile("../tests/kSAT.txt")
+probList = getProblems(equation_list, numProblems)
 
 # c, problem number, max literals in a clause, answer
 # p, cnf, number of variables, number of clauses
