9b6bed4bb9523fba3942a6d247b056d28f5cc8d2
Danielle <daniellec0321@gmail.com>
2022-09-18 19:05:20 -0400

    writeBrute: Combined functions to read file

--- brute/brute.py
+++ brute/brute.py
@@ -11,63 +11,47 @@ class Problem:
 
 
 
-# Function to read file
-def readFile(filename):
-    
+# parse a list into LIST OF CLASS PROBLEMS
+def getProblems(filename):
+
     rFile = open(filename, "r")
 
-    # value to hold number of problems
+    # values to hold problem info
     numProblems = 0
-
-    # array to hold all the info
-    equation_list = list();
-    temp = rFile.readline()
-
-    # read and add to list
-    while (temp):
-        equation_list.append(temp.strip())
-        if (temp[0]) == 'c':
-            numProblems += 1
-        temp = rFile.readline()
-
-    rFile.close()
-    return equation_list, numProblems
-
-
-
-# parse a list into LIST OF CLASS PROBLEMS
-def getProblems(equation_list):
-
     problemList = list()
 
     # initialize readings
-    celems = equation_list[0].split(" ")
-    pelems = equation_list[1].split(" ")
+    celems = rFile.readline().strip().split(" ")
+    pelems = rFile.readline().strip().split(" ")
     currProblem = Problem(celems[1], celems[2], pelems[2], pelems[3], celems[3])
 
-    for counter in range(2, len(equation_list)):
+    currLine = rFile.readline().strip()
+    while(currLine):
 
         # if equation list at counter starts with c, reset
-        if equation_list[counter][0] == 'c':
+        if currLine[0] == 'c':
            
             problemList.append(currProblem)
-            celems = equation_list[counter].split(" ")
+            celems = currLine.split(" ")
             currProblem = Problem(int(celems[1]), int(celems[2]), -1, -1, celems[3])
 
         # if starts with p, set all other class elements
-        elif equation_list[counter][0] == 'p':
+        elif currLine[0] == 'p':
 
-            pelems = equation_list[counter].split(" ")
+            pelems = currLine.split(" ")
             currProblem.numVariables = int(pelems[2])
             currProblem.numClauses = int(pelems[3])
-
+    
+        # otherwise, append equation to current problem
         else: 
 
-            # append equation to list
-            eq = equation_list[counter].split(",")
+            eq = currLine.split(",")
             for i in range(0, len(eq)):
                 eq[i] = int(eq[i])
             currProblem.probArray.append(eq)
+
+        # read next line
+        currLine = rFile.readline().strip()
 
     # append last problem to list
     problemList.append(currProblem)
