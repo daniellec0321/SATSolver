8e54f7b871c8cacbba88cddf1bc4890e11042db8
Danielle <daniellec0321@gmail.com>
2022-09-22 13:44:17 -0400

    backtrack: wrote verifyWFF

--- backtrack/backtrack.py
+++ backtrack/backtrack.py
@@ -16,6 +16,44 @@ class Problem:
 
 
 
+# Class "stack" (makes writing a stack easier for us)
+class Stack:
+
+    def __init__(self):
+        self.__stack = list()
+
+    def push(self, elem):
+        self.__stack.append(elem)
+
+    def top(self):
+        return self.__stack[len(self.__stack)-1]
+
+    def pop(self):
+        return self.__stack.pop()
+
+    def clear(self):
+        self.__stack.clear()
+
+    def empty(self):
+        return len(self.__stack) == 0
+
+    def print(self):
+        print(self.__stack)
+
+
+
+# Class "variable" (holds a current variable value and its flags)
+# vals will always be 0 or 1
+class Var:
+
+    def __init__(self):
+        self.relativeVar = -1
+        self.currValue = -1
+        self.valsTried = list()
+        self.flag = True
+
+
+
 # parse a list into LIST OF CLASS PROBLEMS
 # filename is a string that contains path to file
 # returns a list of Problems
@@ -67,6 +105,73 @@ def getProblems(filename):
 
 
 
+# returns an array of the next assignments to try
+# currProblem is a problem object
+# prevProblemRes is an integer representing what the last verifyWFF outputted
+# currStack is the current stack of vars that we were using
+# def getAssignments(currProblem, prevProblemRes, currStack):
+
+
+
+# Takes a problem and evaluates if it is satisfiable or not
+# currProblem is a problem object
+# assignments is an array of integers representing the current variable assignments
+    # 0 is false, 1 is true, -1 is UNDETERMINED
+# returns a 0 if WFF is unsatisfiable, returns a 1 if satisfiable, returns -1 if undetermined
+def verifyWFF(currProblem, assignments):
+
+    # variable that tests if there is a clause that is undetermined
+    udClause = False
+
+    # loop through the prob array of current problem
+    for clause in currProblem.probArray:
+
+        # represents the return value of the clause: 0 is false, 1 is true, -1 is undetermined
+        clauseRet = -1
+
+        # represents if we had an undetermined var in our clause
+        hasUndetermined = False
+
+        # loop through clause until we hit a true var OR zero
+        for literal in clause:
+
+            # base case: 0
+            if literal == 0:
+                break
+
+            # get absolute value and negation
+            val = abs(literal)
+            negation = (literal < 0)
+
+            # if assignments at this value is -1, then don't test it
+            if (assignments[val-1] == -1):
+                hasUndetermined = True
+                continue
+
+            # test if this value is true
+            if negation:
+                if assignments[val-1] == 0:
+                    clauseRet = True
+                    break
+
+            else:
+                if assignments[val-1] == 1:
+                    clauseRet = True
+                    break
+
+        # if clause ret is still -1 and there were no undetermineds, then its unsatisfiable
+        if clauseRet == -1 and hasUndetermined == False:
+            return 0
+
+        # if clause ret is still -1, then we have an undetermined clause
+        if clauseRet == -1:
+            return -1
+
+    # if we got through every clause, then it is satisfiable
+    return 1
+
+
+
 # This function prints the result of our current problem solver
 # currProblem is a Problem object
 # result is a boolean value representing if the problem was satisfiable or not
@@ -101,6 +206,7 @@ def writeOutput(currProblem, result):
 #################
 # FUNCTION MAIN #
 #################
+"""
 def main():
 
     # Test command line input
@@ -143,3 +249,4 @@ def main():
 
 
 main()
+"""
